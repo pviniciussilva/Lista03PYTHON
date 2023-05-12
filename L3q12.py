c684b946e6843adf412850c42a2fb5468430f50a
pviniciussilva <126494717+pviniciussilva@users.noreply.github.com>
2023-05-12 13:18:08 -0300

    update L3q12.py

---
+++ L3q12.py
@@ -0,0 +1,10 @@
+'''Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10. O usuário deve informar de qual numero ele deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo:
+
+    a. Tabuada de 5:
+       5 X 1 = 5
+       5 X 2 = 10
+       ...
+       5 X 10 = 50'''
+num = int(input('Informe o número de qual tabuada você deseja: '))
+for n in range(1, 11):
+  print(f'{n} x {num} = {n * num}')
