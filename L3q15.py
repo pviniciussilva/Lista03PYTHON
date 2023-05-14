637231ed5c88b5aa7f8bac9cc0fd3759b4aae662
pviniciussilva <126494717+pviniciussilva@users.noreply.github.com>
2023-05-14 19:45:45 -0300

    update L3q15.py

--- L3q15.py
+++ L3q15.py
@@ -1,6 +1,12 @@
 '''A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... Faça um programa capaz de gerar a série até o n−ésimo termo.'''
 numero = 0
 while numero < 90:
-  numero += 1
-  numero += numero
   print(numero)
+  if numero == 3:
+    numero = numero + (numero - 1)
+    print(numero) 
+  else:
+    numero = numero + numero - 
+  numero += 1
+  print(numero)
+  numero = numero + (numero - 1)'''
